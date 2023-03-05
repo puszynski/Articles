58a5dc27225c440c01d4e70c25259bc94d8de44a
Jakub Puszyński <jakub.puszynski@efitness.com>
2023-03-05 11:47:31 +0100

    WholeValuePattern

---
+++ WholeValuePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿class User
+{
+    public int Id { get; set; }
+    public Name Name { get; set; }
+    public Address Address { get; set; }
+}
+
+class Name
+{
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string GetFullName() 
+        => $"{FirstName}({MiddleName ?? string.Empty}) {LastName}";
+}
+
+class Address
+{
+    public string StreetName { get; set; }
+    public int HomeNumber { get; set; }
+    public string City { get; set; }
+    public string CityCode { get; set; }
+    public string Country { get; set; }
+}
